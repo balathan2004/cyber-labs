9bf3688aa78309a5c64479503f7588e57ad23f4c
massedits <balathan2vijay004@gmail.com>
2024-04-14 19:23:41 +0530

    14/4 deploy

--- pages/api/video_action/reply_comment.js
+++ pages/api/video_action/reply_comment.js
@@ -1,4 +1,47 @@
-export default function (req, res) {
-  console.log(req.body);
+import { firestore } from "@/config";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
+
+export default async function (req, res) {
+  const { course_id, video_id, comment_id, comment, comment_user, time } =
+    JSON.parse(req.body);
+
+  var docName = `${course_id}@${video_id}`;
+
+  const docRef = doc(firestore, "comments", docName);
+
+  const docData = (await getDoc(docRef)).data();
+
+  const docComment = docData.comment.find(
+    (file) => file.comment_id == comment_id
+  );
+
+  const otherComments = docData.comment.filter(
+    (file) => file.comment_id != comment_id
+  );
+
+  if (docComment.hasReplies && docComment.hasReplies.length > 0) {
+    const replyData = {
+      comment: comment,
+      comment_id: comment_id,
+      comment_user: comment_user,
+      time: time,
+    };
+
+    docComment.hasReplies.push(replyData);
+    otherComments.push(docComment);
+    await updateDoc(docRef, { comment: otherComments });
+  } else {
+    const replyData = {
+      comment: comment,
+      comment_id: comment_id,
+      comment_user: comment_user,
+      time: time,
+    };
+    docComment.hasReplies = [];
+    docComment.hasReplies.push(replyData);
+    otherComments.push(docComment);
+    await updateDoc(docRef, { comment: otherComments });
+  }
+
   res.json({ message: "comment added", authType: 200 });
 }
