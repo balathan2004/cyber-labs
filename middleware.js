0eb2139236d73bb63992b7848334806e885c31c8
massedits <balathan2vijay004@gmail.com>
2024-05-30 06:55:17 +0530

    admin redirect bug

--- middleware.js
+++ middleware.js
@@ -8,9 +8,8 @@ export default function middleware(request) {
 
   const { pathname } = nextUrl;
 
-  console.log(pathname, cyberLabs_Admin);
-
   if (pathname.startsWith("/admin")) {
+    console.log(pathname, cyberLabs_Admin == process.env.ADMIN_NAME);
     if (cyberLabs_Admin == process.env.ADMIN_NAME && pathname == "/admin") {
       return NextResponse.redirect(new URL("/admin/dashboard", request.url));
     } else if (
